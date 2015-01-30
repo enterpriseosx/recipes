51b0fa14c4d53642ec80dbd227070426857c38be
johnny830 <johnny@enterpriseosx.com>
2015-01-30 11:44:56 -0500

    Delete remove.sh

--- remove.sh
+++
@@ -1,10 +0,0 @@
-#!/bin/bash
-
-for directory in `find . -type d | cut -c3-`
-
-do newdir=`echo $directory | sed s'/.$//'`
-mv $directory $newdir
-done
-exit
-
-#!/bin/bashmodel=system_profiler SPHardwareDataType | awk '/Model Name/ {print $3,$4}'if [ model = "MacBook Pro" ] ; then    laptop="ML-”    serial=ioreg -l | grep IOPlatformSerialNumber | cut -c 37-46    /usr/sbin/scutil --set ComputerName $laptop$serialelse    workstation="MW-”    serial=ioreg -l | grep IOPlatformSerialNumber | cut -c 37-46    /usr/sbin/scutil --set ComputerName $workstation$serial
